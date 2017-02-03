31d0b6dd7fcd26997ff839773e8687438683a59c
jksbbtpoint <jksbbtpoint@gmail.com>
2017-02-03 12:38:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/articleone', function (req, res){
+app.get('/article-one', function (req, res){
     rs.send('Article one is here');
 });
 

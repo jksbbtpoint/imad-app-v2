1af558abb6c1c7360a3ac6c08a7b87f90a14ecf1
jksbbtpoint <jksbbtpoint@gmail.com>
2017-02-03 12:51:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,13 +10,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res){
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article-one', function (req, res){
-    rs.send('Article one is here');
-});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));

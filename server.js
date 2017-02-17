84e8178a5c510bbd6e2e981322896be11bbe4a2a
jksbbtpoint <jksbbtpoint@gmail.com>
2017-02-17 15:13:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-document.writeln("hjhjhjk");
+window.prompt("hjhjhjk");
 var app = express();
 app.use(morgan('combined'));
 

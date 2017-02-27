2b21d10808ee940a4054041a23ca58ab57fcd831
jksbbtpoint <jksbbtpoint@gmail.com>
2017-02-27 16:14:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var Pool =new Pool(config)
+var Pool =new Pool(config);
 app.get('/test-db', function (req, res) {
-  pool.query('SELECT * FROM TEST', function(err, result){
+  pool.query('SELECT * FROM test', function(err, result){
       if(err){
           res.status(500).send(err.toString());
       }else{
